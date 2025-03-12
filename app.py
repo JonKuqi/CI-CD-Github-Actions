409895ec688faf717bd5f19542374fdeae1fb66d
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2025-03-12 13:20:42 +0100

    Flask API completed

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def home():
     return jsonify({"message": "Recommender System API is running!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
