d2d06d7317853d099ebd56d66ccf6afabf93af81
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2025-03-13 09:00:36 +0100

    Final Commit for deployment

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def home():
     return jsonify({"message": "Recommender System API is running!"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
